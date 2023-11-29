5980f03276c6481771137f6a61a915bce64a5283
franSlepoy <slepoyfrancisco@gmail.com>
2023-11-29 11:31:31 -0300

    fotos

--- src/components/pages/litoteca/Litoteca.jsx
+++ src/components/pages/litoteca/Litoteca.jsx
@@ -1,205 +1,393 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import {  Hidden, Typography } from '@mui/material';
-import { useTranslation } from 'react-i18next';
-
-
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { Hidden, Typography } from "@mui/material";
+import { useTranslation } from "react-i18next";
 
 export default function Litoteca() {
-  const [isEnglish, setIsEnglish] = React.useState(true); 
+  const [isEnglish, setIsEnglish] = React.useState(true);
   const { t, i18n } = useTranslation();
 
   const toggleLanguage = () => {
-    i18n.changeLanguage(isEnglish ? 'es' : 'en');
+    i18n.changeLanguage(isEnglish ? "es" : "en");
     setIsEnglish(!isEnglish);
-  }
+  };
   return (
     <>
-<Hidden smDown>
- <Box >
- <Typography
-         
-          
-          onClick={toggleLanguage}  
-          sx={{ 
-            cursor: 'pointer',
-            '&:hover': {
-              textDecoration: 'underline'
-            },
-            
-            mt:"78px", textAlign:"center",  
-            fontSize:"15px", 
-            fontFamily: 'Albert Sans', 
-            fontWeight:"600" 
-          }} 
-          variant="h6"
+      <Hidden smDown>
+        <Box>
+          <Typography
+            onClick={toggleLanguage}
+            sx={{
+              cursor: "pointer",
+              "&:hover": {
+                textDecoration: "underline",
+              },
+
+              mt: "78px",
+              textAlign: "center",
+              fontSize: "15px",
+              fontFamily: "Albert Sans",
+              fontWeight: "600",
+            }}
+            variant="h6"
+          >
+            {isEnglish ? "ESPAÑOL" : "ENGLISH"}
+          </Typography>
+        </Box>
+
+        <Typography
+          color="black"
+          sx={{
+            mt: "100px",
+            textAlign: "center",
+            fontFamily: "Albert Sans",
+            fontSize: "19px",
+            fontWeight: "600",
+            lineHeight: "normal",
+          }}
         >
-          {isEnglish ? 'ESPAÑOL' : 'ENGLISH'}
+          FAIVOVICH & GOLDBERG
         </Typography>
- </Box>
 
- 
-<Typography   color="black"sx={{mt:"100px",textAlign:"center", fontFamily: 'Albert Sans', fontSize:"19px", fontWeight:"600", lineHeight:"normal", }}>
-                        FAIVOVICH & GOLDBERG
-                    </Typography>
+        <Box mb={5} sx={{ mt: "76px", width: "60%", ml: "20%", mr: "20%" }}>
+          <Box width={"372px"} m={"auto"}>
+            <Typography
+              sx={{
+                textAlign: "center",
+                fontSize: "31px",
+                fontWeight: "600",
+                lineHeight: "normal",
+                fontFamily: "Albert Sans",
+              }}
+              variant="h4"
+            >
+              {t("litotecaTitulo1")}
+            </Typography>
+            <Typography
+              sx={{
+                mt: "22px",
+                textAlign: "center",
+                fontSize: "13px",
+                fontWeight: "500",
+                fontFamily: "Albert Sans",
+              }}
+              variant="subtitle2"
+            >
+              {t("litotecaTitulo2")}
+            </Typography>
+            <Typography
+              sx={{
+                mt: "22px",
+                textAlign: "center",
+                fontSize: "13px",
+                fontFamily: "Albert Sans",
+                fontWeight: "500",
+              }}
+              variant="h6"
+            >
+              {t("litotecaTitulo3")}
+            </Typography>
+            <Typography
+              sx={{
+                mt: "22px",
+                textAlign: "center",
+                fontSize: "13px",
+                fontWeight: "500",
+                fontFamily: "Albert Sans",
+              }}
+              variant="subtitle2"
+            >
+              {t("litotecaTitulo4")}
+            </Typography>
+            <Typography
+              sx={{
+                textAlign: "center",
+                fontSize: "13px",
+                fontWeight: "500",
+                fontFamily: "Albert Sans",
+              }}
+              variant="subtitle2"
+            >
+              {t("litotecaTitulo5")}
+            </Typography>
+          </Box>
 
+          <Box sx={{ display: "flex", justifyContent: "center", mt: "84px" }}>
+            <Box textAlign={"justify"} width={"49%"}>
+              <Typography
+                sx={{
+                  textIndent: 42,
+                  fontSize: "15px",
+                  fontWeight: "500",
+                  lineHeight: "23px",
+                  fontFamily: "Albert Sans",
+                  width: "100%",
+                }}
+              >
+                {t("litotecaTexto1")}
+              </Typography>
+              <Typography
+                sx={{
+                  textIndent: 42,
+                  fontSize: "15px",
+                  fontWeight: "500",
+                  lineHeight: "23px",
+                  fontFamily: "Albert Sans",
+                  width: "100%",
+                }}
+              >
+                {t("litotecaTexto2")}
+              </Typography>
+              <Typography
+                sx={{
+                  textIndent: 42,
+                  fontSize: "15px",
+                  fontWeight: "500",
+                  lineHeight: "23px",
+                  fontFamily: "Albert Sans",
+                  width: "100%",
+                }}
+              >
+                {t("litotecaTexto3")}
+              </Typography>
+              <Typography
+                sx={{
+                  textIndent: 42,
+                  fontSize: "15px",
+                  fontWeight: "500",
+                  lineHeight: "23px",
+                  fontFamily: "Albert Sans",
+                  width: "100%",
+                }}
+              >
+                {t("litotecaTexto4")}
+              </Typography>
+            </Box>
 
-<Box  mb={5} sx={{mt:"76px",width:"60%", ml:"20%", mr:"20%"}} >
+            <Box textAlign={"justify"} ml={"4%"} width={"49%"}>
+              <Typography
+                sx={{
+                  textIndent: 42,
+                  fontSize: "15px",
+                  fontWeight: "500",
+                  lineHeight: "23px",
+                  fontFamily: "Albert Sans",
+                  width: "100%",
+                  ml: 2,
+                }}
+              >
+                {t("litotecaTexto6")}
+              </Typography>
+              <Typography
+                sx={{
+                  textIndent: 42,
+                  fontSize: "15px",
+                  fontWeight: "500",
+                  lineHeight: "23px",
+                  fontFamily: "Albert Sans",
+                  width: "100%",
+                  ml: 2,
+                }}
+              >
+                {t("litotecaTexto7")}
+              </Typography>
+              <Typography
+                sx={{
+                  textIndent: 42,
+                  fontSize: "15px",
+                  fontWeight: "500",
+                  lineHeight: "23px",
+                  fontFamily: "Albert Sans",
+                  width: "100%",
+                  ml: 2,
+                }}
+              >
+                {t("litotecaTexto8")}
+              </Typography>
+            </Box>
+          </Box>
+        </Box>
 
+        <Box
+          sx={{
+            width: "60%",
+            ml: "20%",
+            mr: "20%",
+            margin: "auto",
+            overflowY: "clip",
+          }}
+        >
+          <Box textAlign={"center"}>
+            <img
+              style={{ marginTop: "80px" }}
+              width={"100%"}
+              src="imagenes/lito/lito1.png"
+              alt=""
+            />
+          </Box>
 
-<Box width={"372px"} m={"auto"}>
-<Typography sx={{textAlign:"center", fontSize:"31px", fontWeight:"600",lineHeight:"normal", fontFamily: 'Albert Sans', }} variant="h4">{t("litotecaTitulo1")}</Typography>
-  <Typography sx={{mt:"22px", textAlign:"center",  fontSize:"13px", fontWeight:"500", fontFamily: 'Albert Sans' }}  variant="subtitle2">{t("litotecaTitulo2")}</Typography>
-  <Typography sx={{ mt:"22px", textAlign:"center",  fontSize:"13px", fontFamily: 'Albert Sans', fontWeight:"500" }} variant="h6">{t("litotecaTitulo3")}</Typography>
-  <Typography sx={{ mt:"22px", textAlign:"center",  fontSize:"13px", fontWeight:"500", fontFamily: 'Albert Sans', }} variant="subtitle2">
-  {t("litotecaTitulo4")}
+          <Box mb={"290px"} textAlign={"center"}>
+            <img
+              style={{ marginTop: "180px" }}
+              width={"100%"}
+              src="imagenes/lito/lito2.png"
+              alt=""
+            />
+          </Box>
+        </Box>
+      </Hidden>
 
-</Typography>
-<Typography sx={{ textAlign:"center", fontSize:"13px", fontWeight:"500", fontFamily: 'Albert Sans' }} variant="subtitle2">
-  {t("litotecaTitulo5")}
+      <Hidden smUp>
+        <Box sx={{ ml: 2, mr: 2, mt: "25%", maxWidth: "90%" }}>
+          <Box sx={{ mb: 3 }}>
+            <Typography
+              sx={{
+                fontSize: 19,
+                fontWeight: "600",
+                fontFamily: "Albert Sans",
+              }}
+              variant="h4"
+            >
+              {t("litotecaTitulo1")}
+            </Typography>
+            <Typography
+              sx={{ fontSize: 15, fontFamily: "Albert Sans" }}
+              variant="subtitle2"
+            >
+              {t("litotecaTitulo2")}
+            </Typography>
+            <Typography
+              sx={{
+                width: "90%",
+                mt: 2,
+                fontSize: 15,
+                fontFamily: "Albert Sans",
+              }}
+              variant="h6"
+            >
+              {t("litotecaTitulo3")}
+            </Typography>
+            <Typography
+              sx={{ mt: 2, fontSize: 15, fontFamily: "Albert Sans" }}
+              variant="subtitle2"
+            >
+              {t("litotecaTitulo4")}
+            </Typography>
+            <Typography
+              sx={{ fontSize: 15, fontFamily: "Albert Sans" }}
+              variant="h6"
+            >
+              {t("litotecaTitulo5")}
+            </Typography>
 
-</Typography>
+            <Box textAlign={"justify"}>
+              <Typography
+                sx={{
+                  mt: 2,
+                  fontSize: 15,
+                  fontFamily: "Albert Sans",
+                  width: "100%",
+                }}
+              >
+                {t("litotecaTexto1")}
+              </Typography>
 
+              <Typography
+                sx={{
+                  fontSize: "15px",
+                  fontFamily: "Albert Sans",
+                  width: "100%",
+                }}
+              >
+                {t("litotecaTexto2")}
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: "15px",
+                  fontFamily: "Albert Sans",
+                  width: "100%",
+                }}
+              >
+                {t("litotecaTexto3")}
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: "15px",
+                  fontFamily: "Albert Sans",
+                  width: "100%",
+                }}
+              >
+                {t("litotecaTexto4")}
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: "15px",
+                  fontFamily: "Albert Sans",
+                  width: "100%",
+                }}
+              >
+                {t("litotecaTexto4")}
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: "15px",
+                  fontFamily: "Albert Sans",
+                  width: "100%",
+                }}
+              >
+                {t("litotecaTexto5")}
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: "15px",
+                  fontFamily: "Albert Sans",
+                  width: "100%",
+                }}
+              >
+                {t("litotecaTexto6")}
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: "15px",
+                  fontFamily: "Albert Sans",
+                  width: "100%",
+                }}
+              >
+                {t("litotecaTexto7")}
+              </Typography>
+              <Typography
+                sx={{
+                  fontSize: "15px",
+                  fontFamily: "Albert Sans",
+                  width: "100%",
+                }}
+              >
+                {t("litotecaTexto8")}
+              </Typography>
+            </Box>
+          </Box>
 
-</Box>
-  
+          <Box sx={{ margin: "auto", overflowY: "clip" }}>
+            <Box textAlign={"center"}>
+              <img
+                style={{ marginTop: "40px" }}
+                width={"100%"}
+                src="imagenes/lito/lito1.png"
+                alt=""
+              />
+            </Box>
 
-<Box  sx={{display: "flex", justifyContent:"center", mt:"84px"   }}>
-   <Box textAlign={"justify"} width={"49%"}>
-   <Typography sx={{textIndent:42,fontSize:"15px", fontWeight:"500",lineHeight:"23px", fontFamily: 'Albert Sans', width:"100%"}}>
-   {t("litotecaTexto1")}
-
-</Typography>
-<Typography sx={{textIndent:42,fontSize:"15px", fontWeight:"500",lineHeight:"23px", fontFamily: 'Albert Sans', width:"100%"}}>
-{t("litotecaTexto2")}
-
-    </Typography>
-    <Typography sx={{textIndent:42,fontSize:"15px", fontWeight:"500",lineHeight:"23px", fontFamily: 'Albert Sans', width:"100%"}}>
-{t("litotecaTexto3")}
-
-    </Typography>
-    <Typography sx={{textIndent:42,fontSize:"15px", fontWeight:"500",lineHeight:"23px", fontFamily: 'Albert Sans', width:"100%"}}>
-{t("litotecaTexto4")}
-
-    </Typography>
-   </Box>
-   
-   <Box textAlign={"justify"} ml={"4%"} width={"49%"}>
-   <Typography sx={{textIndent:42,fontSize:"15px", fontWeight:"500",lineHeight:"23px", fontFamily: 'Albert Sans', width:"100%", ml:2}}> 
-   {t("litotecaTexto6")}
-
-    </Typography>
-    <Typography sx={{textIndent:42,fontSize:"15px", fontWeight:"500",lineHeight:"23px", fontFamily: 'Albert Sans', width:"100%", ml:2}}> 
-    {t("litotecaTexto7")}
-
-    </Typography>
-    <Typography sx={{textIndent:42,fontSize:"15px", fontWeight:"500",lineHeight:"23px", fontFamily: 'Albert Sans', width:"100%", ml:2}}> 
-    {t("litotecaTexto8")}
-
-    </Typography>
-   </Box>
-   
-</Box>
-</Box>
-
-
-
-
-<Box sx={{ width:"60%", ml:"20%", mr:"20%", margin:"auto",  overflowY: "clip" }}>
-
-<Box textAlign={"center"}>
-<img style={{marginTop:"80px"}} width={"100%"}  src="imagenes/lito/lito1.png" alt="" />
-</Box>
-
-
-
-<Box mb={"290px"}  textAlign={"center"}>
-<img style={{marginTop:"180px"}} width={"100%"}  src="imagenes/lito/lito2.png" alt="" />
-</Box>
-
-
-
-</Box>
-</Hidden>
-  
-
-<Hidden smUp>
-<Box sx={{ml:2, mr:2,mt:"25%", maxWidth:"90%"}}>
-<Box sx={{mb:3}}>
-<Typography sx={{ fontSize:19,fontWeight:"600", fontFamily: 'Albert Sans', }} variant="h4">{t("litotecaTitulo1")}</Typography>
-  <Typography sx={{ fontSize:15, fontFamily: 'Albert Sans' }} variant="subtitle2">{t("litotecaTitulo2")}</Typography>
-  <Typography sx={{width:"90%",  mt:2, fontSize:15, fontFamily: 'Albert Sans' }} variant="h6">{t("litotecaTitulo3")}</Typography>
-  <Typography sx={{ mt:2, fontSize:15, fontFamily: 'Albert Sans' }} variant="subtitle2">
-  {t("litotecaTitulo4")}
-  </Typography>
-  <Typography sx={{ fontSize:15, fontFamily: 'Albert Sans' }} variant="h6">{t("litotecaTitulo5")}</Typography>
-  
-  
-
-<Box textAlign={"justify"}>
-<Typography sx={{  mt:2,fontSize:15, fontFamily: 'Albert Sans', width:"100%"}}> 
-{t("litotecaTexto1")}
-
-</Typography>
-
-<Typography sx={{fontSize:"15px", fontFamily: 'Albert Sans', width:"100%"}}>
-{t("litotecaTexto2")}
-
-</Typography>
-<Typography sx={{fontSize:"15px", fontFamily: 'Albert Sans', width:"100%"}}>
-{t("litotecaTexto3")}
-
-
-</Typography>
-<Typography sx={{fontSize:"15px", fontFamily: 'Albert Sans', width:"100%"}}>
-{t("litotecaTexto4")}
-
-</Typography>
-<Typography sx={{fontSize:"15px", fontFamily: 'Albert Sans', width:"100%"}}>
-{t("litotecaTexto4")}
-
-</Typography>
-<Typography sx={{fontSize:"15px", fontFamily: 'Albert Sans', width:"100%"}}>
-{t("litotecaTexto5")}
-
-</Typography>
-<Typography sx={{fontSize:"15px", fontFamily: 'Albert Sans', width:"100%"}}>
-{t("litotecaTexto6")}
-
-</Typography>
-<Typography sx={{fontSize:"15px", fontFamily: 'Albert Sans', width:"100%"}}>
-{t("litotecaTexto7")}
-
-</Typography>
-<Typography sx={{fontSize:"15px", fontFamily: 'Albert Sans', width:"100%"}}>
-{t("litotecaTexto8")}
-
-</Typography>
-</Box>
-
-</Box>
-
-<Box sx={{  margin:"auto",  overflowY: "clip" }}>
-<Box textAlign={"center"}>
-<img style={{marginTop:"40px"}} width={"100%"}  src="imagenes/lito/lito1.png" alt="" />
-</Box>
-
-
-
-<Box mb={"290px"}  textAlign={"center"}>
-<img style={{marginTop:"80px"}} width={"100%"}  src="imagenes/lito/lito2.png" alt="" />
-</Box>
-
-
-
-
-</Box>
-
-
-</Box>
-</Hidden>
-</>
-);
-  
-    
+            <Box mb={"290px"} textAlign={"center"}>
+              <img
+                style={{ marginTop: "80px" }}
+                width={"100%"}
+                src="imagenes/lito/lito2.png"
+                alt=""
+              />
+            </Box>
+          </Box>
+        </Box>
+      </Hidden>
+    </>
+  );
 }
